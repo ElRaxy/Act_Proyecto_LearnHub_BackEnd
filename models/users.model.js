9ee738799c7074bc5a4ee62d4aa47656fd8f20ba
blanck-2024 <alejandropuentegimenez2@gmail.com>
2025-11-26 17:49:37 +0100

    cambios locales en users.model.js

--- models/users.model.js
+++ models/users.model.js
@@ -69,6 +69,6 @@ const usersSchema = new mongoose.Schema({
   },
 })
 
-const usersModel = mongoose.model('                             ', usersSchema)
+const usersModel = mongoose.model('users', usersSchema)
 
 module.exports = usersModel
