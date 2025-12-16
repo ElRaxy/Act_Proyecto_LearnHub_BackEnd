029f42d8da1845f09391f5150b98627e32230381
blanck-2024 <alejandropuentegimenez2@gmail.com>
2025-12-16 16:17:43 +0100

    Creando la carpeta api

---
+++ controllers/api/course.api.controller.js
@@ -0,0 +1,85 @@
+const Course = require('../models/courses.model')
+const baseUrlCourses = `/api/${process.env.API_VERSION}/courses`
+
+// GET ALL COURSES (vista)
+exports.getAllCourses = async (req, res) => {
+    try {
+        const courses = await Course.find()
+
+        res.render('courses/index', {
+            title: 'Cursos disponibles',
+            courses,
+            baseApi: baseUrlCourses
+        })
+    } catch (error) {
+        res.status(500).send('Error obteniendo cursos: ' + error)
+    }
+}
+
+// GET BY ID (vista)
+exports.getCourseById = async (req, res) => {
+    try {
+        const course = await Course.findById(req.params.id)
+
+        res.render('courses/show', {
+            title: course.name,
+            course,
+            baseApi: baseUrlCourses
+        })
+    } catch (error) {
+        res.status(500).send('Error obteniendo curso: ' + error)
+    }
+}
+
+// FORM NEW (vista)
+exports.showCreateForm = (req, res) => {
+    res.render('courses/new', {
+        title: 'Nuevo curso',
+        baseApi: baseUrlCourses
+    })
+}
+
+// CREATE (acción)
+exports.createCourse = async (req, res) => {
+    try {
+        await Course.create(req.body)
+        res.redirect(baseUrlCourses)
+    } catch (error) {
+        res.status(500).send('Error creando curso: ' + error)
+    }
+}
+
+// FORM EDIT (vista)
+exports.showEditForm = async (req, res) => {
+    try {
+        const course = await Course.findById(req.params.id)
+
+        res.render('courses/edit', {
+            title: 'Editar curso',
+            course,
+            baseApi: baseUrlCourses
+        })
+    } catch (error) {
+        res.status(500).send('Error obteniendo datos: ' + error)
+    }
+}
+
+// UPDATE (acción)
+exports.updateCourse = async (req, res) => {
+    try {
+        await Course.findByIdAndUpdate(req.params.id, req.body)
+        res.redirect(`${baseUrlCourses}/${req.params.id}`)
+    } catch (error) {
+        res.status(500).send('Error actualizando curso: ' + error)
+    }
+}
+
+// DELETE (acción)
+exports.deleteCourse = async (req, res) => {
+    try {
+        await Course.findByIdAndDelete(req.params.id)
+        res.redirect(baseUrlCourses)
+    } catch (error) {
+        res.status(500).send('Error eliminando curso: ' + error)
+    }
+}
---
+++ controllers/api/enrollement.api.controller.js
@@ -0,0 +1,90 @@
+const enrollmentService = require('../services/enrollment.service')
+const userService = require('../services/user.service')
+const courseService = require('../services/course.service')
+const baseUrlEnrollments = `/api/${process.env.API_VERSION}/enrollments`
+
+// Listar todas las matrículas
+exports.getAllEnrollments = async (req, res) => {
+  try {
+    const enrollments = await enrollmentService.getAllEnrollments()
+    res.locals.tituloEJS = 'Matriculas'
+    res.render('enrollments/index', { enrollments, baseApi: baseUrlEnrollments })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('No se pudieron listar las matriculas')
+  }
+}
+
+// Formulario nueva matrícula
+exports.showNewEnrollment = async (req, res) => {
+  try {
+    const users = await userService.getAllUsers()
+    const courses = await courseService.getAllCourses()
+    res.locals.tituloEJS = 'Nueva Matricula'
+    res.render('enrollments/new', { baseApi: baseUrlEnrollments, users, courses })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error al cargar la vista de nueva matrícula')
+  }
+}
+
+// Crear matrícula
+exports.createEnrollment = async (req, res) => {
+  try {
+    await enrollmentService.createEnrollment(req.body)
+    res.redirect(baseUrlEnrollments)
+  } catch (error) {
+    console.error(error)
+    res.status(400).send(error.message)
+  }
+}
+
+// Formulario editar matrícula
+exports.showEditEnrollment = async (req, res) => {
+  try {
+    const enrollment = await enrollmentService.getEnrollmentById(req.params.id)
+    if (!enrollment) return res.status(404).send('No se encontró la matricula')
+    const users = await userService.getAllUsers()
+    const courses = await courseService.getAllCourses()
+    res.locals.tituloEJS = 'Editar Matricula'
+    res.render('enrollments/edit', { enrollment, baseApi: baseUrlEnrollments, users, courses })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error al cargar la vista de edición')
+  }
+}
+
+// Actualizar matrícula
+exports.updateEnrollment = async (req, res) => {
+  try {
+    await enrollmentService.updateEnrollment(req.params.id, req.body)
+    res.redirect(`${baseUrlEnrollments}/${req.params.id}`)
+  } catch (error) {
+    console.error(error)
+    res.status(400).send(error.message)
+  }
+}
+
+// Eliminar matrícula
+exports.deleteEnrollment = async (req, res) => {
+  try {
+    await enrollmentService.deleteEnrollment(req.params.id)
+    res.redirect(baseUrlEnrollments)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error al eliminar la matricula')
+  }
+}
+
+// Detalle matrícula
+exports.getEnrollmentById = async (req, res) => {
+  try {
+    const enrollment = await enrollmentService.getEnrollmentById(req.params.id)
+    if (!enrollment) return res.status(404).send('No se encontró la matricula')
+    res.locals.tituloEJS = 'Detalle de la Matricula'
+    res.render('enrollments/show', { enrollment, baseApi: baseUrlEnrollments })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error al obtener la matricula')
+  }
+}
