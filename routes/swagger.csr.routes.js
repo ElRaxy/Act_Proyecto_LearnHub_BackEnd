85ac4eb93e981deb73f23f2ef4c66e0b6319373a
Alex Mico <alexmico2006@gmail.com>
2025-12-16 16:53:56 +0100

    Creacion de swagger

---
+++ routes/swagger.csr.routes.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+const courseController = require('../controllers/course.controller')
+const enrollmentController = require('../controllers/enrollement.controller')
+const userController = require('../controllers/user.controller')
+
