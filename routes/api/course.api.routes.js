6daeb45d4296c5bf25ef65ee8cbac0f84bed7bae
sergiowin <sergiomartinezamoros@gmail.com>
2025-12-17 17:34:35 +0100

    Creacion de rutas api de course

--- routes/api/course.api.routes.js
+++ routes/api/course.api.routes.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const enrollementApiController = require('../../controllers/api/enrollement.api.controller')
+const courseApiController = require('../../controllers/api/course.api.controller')
 
+// API JSON
+router.get('/', courseApiController.getAllCourses)
+router.post('/', courseApiController.createCourse)
+router.get('/show/:id', courseApiController.getById)
+router.put('/:id', courseApiController.editCourse)
+router.delete('/:id', courseApiController.deleteCourse)
 
 module.exports = router
