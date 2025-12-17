3b1461a3b9851468bcc09a6006b30886ab67273e
sergiowin <sergiomartinezamoros@gmail.com>
2025-12-17 16:31:37 +0100

    Cambio courses render por status y send por json

--- controllers/api/course.api.controller.js
+++ controllers/api/course.api.controller.js
@@ -5,14 +5,9 @@ const baseUrlCourses = `/api/${process.env.API_VERSION}/courses`
 exports.getAllCourses = async (req, res) => {
     try {
         const courses = await Course.find()
-
-        res.render('courses/index', {
-            title: 'Cursos disponibles',
-            courses,
-            baseApi: baseUrlCourses
-        })
+        res.status(200).json(courses)
     } catch (error) {
-        res.status(500).send('Error obteniendo cursos: ' + error)
+        res.status(500).json('Error obteniendo cursos: ' + error)
     }
 }
 
@@ -20,32 +15,24 @@ exports.getAllCourses = async (req, res) => {
 exports.getCourseById = async (req, res) => {
     try {
         const course = await Course.findById(req.params.id)
-
-        res.render('courses/show', {
-            title: course.name,
-            course,
-            baseApi: baseUrlCourses
-        })
+        res.status(200).json(course)
     } catch (error) {
-        res.status(500).send('Error obteniendo curso: ' + error)
+        res.status(500).json('Error obteniendo curso: ' + error)
     }
 }
 
 // FORM NEW (vista)
 exports.showCreateForm = (req, res) => {
-    res.render('courses/new', {
-        title: 'Nuevo curso',
-        baseApi: baseUrlCourses
-    })
+    res.status(200).json(users)
 }
 
 // CREATE (acción)
 exports.createCourse = async (req, res) => {
     try {
         await Course.create(req.body)
-        res.redirect(baseUrlCourses)
+        res.status(201).json(Course)
     } catch (error) {
-        res.status(500).send('Error creando curso: ' + error)
+        res.status(500).json('Error creando curso: ' + error)
     }
 }
 
@@ -53,14 +40,9 @@ exports.createCourse = async (req, res) => {
 exports.showEditForm = async (req, res) => {
     try {
         const course = await Course.findById(req.params.id)
-
-        res.render('courses/edit', {
-            title: 'Editar curso',
-            course,
-            baseApi: baseUrlCourses
-        })
+        res.status(200).json(course)
     } catch (error) {
-        res.status(500).send('Error obteniendo datos: ' + error)
+        res.status(500).json('Error obteniendo datos: ' + error)
     }
 }
 
@@ -68,9 +50,10 @@ exports.showEditForm = async (req, res) => {
 exports.updateCourse = async (req, res) => {
     try {
         await Course.findByIdAndUpdate(req.params.id, req.body)
-        res.redirect(`${baseUrlCourses}/${req.params.id}`)
+       // res.redirect(`${baseUrlCourses}/${req.params.id}`)
+        res.status(200).json(updatedCourse)
     } catch (error) {
-        res.status(500).send('Error actualizando curso: ' + error)
+        res.status(500).json('Error actualizando curso: ' + error)
     }
 }
 
@@ -78,8 +61,9 @@ exports.updateCourse = async (req, res) => {
 exports.deleteCourse = async (req, res) => {
     try {
         await Course.findByIdAndDelete(req.params.id)
-        res.redirect(baseUrlCourses)
+        res.status(200).json(user)
+       // res.redirect(baseUrlCourses)
     } catch (error) {
-        res.status(500).send('Error eliminando curso: ' + error)
+        res.status(500).json('Error eliminando curso: ' + error)
     }
 }
