096476117dad7dab1a6cb300ca6beaf46f52eba3
blanck-2024 <alejandropuentegimenez2@gmail.com>
2026-02-04 19:28:38 +0100

    Agregando los middleware de los routes.api

--- routes/api/enrollement.api.routes.js
+++ routes/api/enrollement.api.routes.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const router = express.Router()
 const enrollementApiController = require('../../controllers/api/enrollement.api.controller')
-
+const { verifyToken } = require('../../middlewares/jwt.mw')
+const { authorize } = require('../../middlewares/role.mw')
 
 // API JSON
-router.get('/', enrollementApiController.getAllEnrollments)
-router.post('/', enrollementApiController.createEnrollment)
-router.get('/show/:id', enrollementApiController.getEnrollmentById)
-router.put('/:id', enrollementApiController.updateEnrollment)
-router.delete('/:id', enrollementApiController.deleteEnrollment)
+router.get(
+  '/',
+  verifyToken,
+  authorize('ADMINISTRADOR', 'PROFESOR', 'ALUMNO'),
+  enrollementApiController.getAllEnrollments
+)
+router.post(
+  '/',
+  verifyToken,
+  authorize('ADMINISTRADOR', 'PROFESOR'),
+  enrollementApiController.createEnrollment
+)
+router.get(
+  '/show/:id',
+  verifyToken,
+  authorize('ADMINISTRADOR', 'PROFESOR', 'ALUMNO'),
+  enrollementApiController.getEnrollmentById
+)
+router.put(
+  '/:id',
+  verifyToken,
+  authorize('ADMINISTRADOR', 'PROFESOR'),
+  enrollementApiController.updateEnrollment
+)
+router.delete(
+  '/:id',
+  verifyToken,
+  authorize('ADMINISTRADOR'),
+  enrollementApiController.deleteEnrollment
+)
 
 module.exports = router
--- routes/api/user.api.routes.js
+++ routes/api/user.api.routes.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const router = express.Router()
 const userApiController = require('../../controllers/api/user.api.controller')
+const { verifyToken } = require('../../middlewares/jwt.mw')
+const { authorize } = require('../../middlewares/role.mw')
 
 // API JSON
-router.get('/', userApiController.getAllUsers)
+router.get(
+  '/',
+  verifyToken,
+  authorize('ADMINISTRADOR', 'PROFESOR', 'ALUMNO'),
+  userApiController.getAllUsers
+)
 router.post('/', userApiController.createUser)
-router.get('/show/:id', userApiController.getById)
-router.put('/:id', userApiController.editUser)
-router.delete('/:id', userApiController.deleteUser)
+router.get(
+  '/show/:id',
+  verifyToken,
+  authorize('ADMINISTRADOR', 'PROFESOR', 'ALUMNO'),
+  userApiController.getById
+)
+router.put(
+  '/:id',
+  verifyToken,
+  authorize('ADMINISTRADOR', 'PROFESOR', 'ALUMNO'),
+  userApiController.editUser
+)
+router.delete(
+  '/:id',
+  verifyToken,
+  authorize('ADMINISTRADOR'),
+  userApiController.deleteUser
+)
 
 module.exports = router
