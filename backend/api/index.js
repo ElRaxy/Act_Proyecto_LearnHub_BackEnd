76b1982bea1cb3b749ea838fa6748c3be5f4164e
Alex Mico <alexmico2006@gmail.com>
2026-02-16 20:48:17 +0100

    chore(repo): keep backend-only structure at repository root

--- backend/api/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('../app')
-const { conectarMongoDB } = require('../utils/mongodb.config')
-
-module.exports = async (req, res) => {
-  await conectarMongoDB()
-  return app(req, res)
-}
